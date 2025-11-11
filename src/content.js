04954e8de3dd24552481e0cc32c0c1fc1e72ec56
Shane Cardoz M <shanecardoz26@gmail.com>
2025-11-11 15:24:17 +0530

    initial of style.css

--- src/content.js
+++ src/content.js
@@ -1,3 +1,3 @@
-// Content script placeholder (v0.1)
-// Nothing here yet. Styling is handled by styles.css for now.
-
+// // NightShift Lite (v0.1)
+// Currently all dark mode styling is handled in style.c
+console.log("NightShift Lite active on:", window.location.hostname);
